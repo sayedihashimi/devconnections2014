5bf4903c304e117c644ef94569cb4e262586c2ef
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2014-09-17 02:00:35 -0700

    updating

--- extensibilty-for-asp.net-devs/ContosoTemplatePack/MvcBasic/Controllers/HomeController.cs
+++ extensibilty-for-asp.net-devs/ContosoTemplatePack/MvcBasic/Controllers/HomeController.cs
@@ -1,10 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿namespace MvcBasic.Controllers {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
 
-namespace MvcBasic.Controllers {
     public class HomeController : ContosoShared.ContosoMvcController {
         public ActionResult Index() {
             return View();
